fb1e9358cddf3c86e635e51e85c4d1d8ab94b580
Cesar Whatley <julceswhat@gmail.com>
2023-08-21 15:29:11 -0500

    Adding code structure

---
+++ src/components/Home.tsx
@@ -0,0 +1,20 @@
+import UserList from "./UserList";
+import useFetch from "../useFetch";
+
+const Home = (): JSX.Element => {
+  const {
+    data: users,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/users");
+
+  return (
+    <section>
+      {error && <p>{error}</p>}
+      {isPending && <p>Loading users...</p>}
+      {users && <UserList users={users} />}
+    </section>
+  );
+};
+
+export default Home;
